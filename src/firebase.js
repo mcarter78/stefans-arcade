5457e5b2a79c44764cfa85b817d68a00a437241a
Matt Carter <mcarter@802secure.com>
2021-03-20 10:42:37 -0700

    change firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAjh52hDjsu9lM79-6xFrwcBK0Y3qNG7F8",
-    authDomain: "arcade-map-8ae23.firebaseapp.com",
-    databaseURL: "https://arcade-map-8ae23.firebaseio.com",
-    projectId: "arcade-map-8ae23",
-    storageBucket: "arcade-map-8ae23.appspot.com",
-    messagingSenderId: "968643604152",
-    appId: "1:968643604152:web:64d340670cdeccc3410e4d",
-    measurementId: "G-Y26EDC60W3"
+    apiKey: "AIzaSyAjh52hDjsu9lM79-AIzaSyCkOnOULpNrBTnHx4l6HuVR3jGyorUh2hw",
+    authDomain: "stefans-arcade.firebaseapp.com",
+    databaseURL: "https://stefans-arcade.uswest2.firebaseio.com",
+    projectId: "stefans-arcade",
+    storageBucket: "stefans-arcade.appspot.com",
+    messagingSenderId: "451170804398",
+    appId: "1:451170804398:web:9d2d5727af056844ee833f",
 };
 
 firebase.initializeApp(firebaseConfig);
